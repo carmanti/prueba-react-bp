73244eb3475ce5048e38921e052c2851fd83b9e7
carmanti <mantilla.carlosjoselo@gmail.com>
2022-12-27 12:22:18 -0500

    agrego validacion de url y mensaje de error

--- src/InputForm/index.js
+++ src/InputForm/index.js
@@ -5,12 +5,18 @@ import React, { useState } from "react";
 function ImageList() {
   const [url, setUrl] = useState("");
   const [imageUrls, setImageUrls] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleUrlChange = (event) => {
     setUrl(event.target.value);
+    setErrorMessage("");
   };
 
   const handleAddClick = () => {
+    if (!urlValida(url)) {
+      setErrorMessage(alert("URL, No valida"));
+      return;
+    }
     setImageUrls([...imageUrls, url]);
     setUrl("");
   };
@@ -34,7 +40,25 @@ function ImageList() {
         </button>
       </div>
       <div className="list">
-        <ul>
+        {errorMessage && <p>{errorMessage}</p>}
+        {imageUrls.length === 0 ? (
+          <p>No posee Gif</p>
+        ) : (
+          <ul>
+            {imageUrls.map((url, index) => (
+              <li key={index}>
+                <img src={url} alt={url} />
+                <button
+                  className="button__eliminar"
+                  onClick={() => handleRemoveClick(index)}
+                >
+                  Eliminar
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+        {/* <ul>
           {imageUrls.map((url, index) => (
             <li key={index}>
               <img src={url} alt={url} />
@@ -46,10 +70,14 @@ function ImageList() {
               </button>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </>
   );
+}
+function urlValida(url) {
+  const urlPattern = /^https?:\/\//;
+  return urlPattern.test(url);
 }
 
 export default ImageList;
